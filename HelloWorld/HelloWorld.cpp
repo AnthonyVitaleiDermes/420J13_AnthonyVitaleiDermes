41b0c512061f09b7f695aec0f470f819bfd12135
Anthony <vitaleidermesanthony@gmail.com>
2025-01-28 18:56:02 -0500

    Finished and added some more comments to understand the basic stuff more ;)

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,92 +1,84 @@
-// HelloWorld.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 #include <string>
 #include <iostream> 
-using namespace std;
+using namespace std; // so we dont have to repeat everytime for std: cout <<
 
-class UsersAccount 
+class UsersAccount
 {
 private:
-
-	// my variables
-
-	int ID;
-	string UserName;
-	string Password;
+    // My variables
+    int ID;
+    string UserName;
+    string Password;
 
 public:
+    // Constructor
+    UsersAccount(int id, const string& username, const string& pwd)
+        : ID(id), UserName(username), Password(pwd) {}
 
-	//Constructor
-	UsersAccount(int id, const string& username, const string& pwd) : ID(id), UserName(username), Password (pwd){}
+    // Login
+    bool Login(const string& CreatedUserName, const string& CreatedPassword) const
+    {
+        return (CreatedUserName == UserName && CreatedPassword == Password);   //started with sudo code then just used c++ :)
+    }
 
-	//Login
-	bool Login(const string CreatedUserName, const string CreatedPassword) //const ???
-	{
-		return (CreatedUserName == UserName && CreatedPassword == Password);
-	}
+    // Getter for ID
+    int getID() const
+    {
+        return ID;
+    }
 };
 
-
 int main()
-{    
-	int ID;
-	string UserName;
-	string Password;
+{
+    int ID;
+    string Password;
+    string UserName;
+    
+    cout << "Welcome to the Login Page !\n ";    // easy display message
+    cout <<"Enter Account ID\n ";                 //display again
+    cin >> ID;   
+    cin.ignore();
+
+    // Entering userName
+    cout << "Enter User Name: ";
+    getline(cin, UserName);
+
+    // Basically copy paste but Password instead of user name
+    cout << "Enter Password: ";
+    getline(cin, Password);
 
 
-	cout << "Welcome to the Login Page\n ! ";    //easy display message
-	cout << "Enter Account ID\n ";
-	cin >> ID;
-	cin.ignore();  
+    //class with the vairables
+    UsersAccount user(ID, UserName, Password);
 
-	//entering userName
-	cout << "Enter User Name: ";
-	getline(cin, UserName);
+    // Brings up the Login
+    cout << "\nLogin:\n";
 
+    while (true)
+    {
+        //redefining the strings
+        string CreatedUserName, CreatedPassword;
 
-	//Basically Copy paste but Password instead of user name
-	cout << "Enter Password: ";
-	getline(cin, Password);
+        // UserName Login
+        cout << "Enter Username: ";
+        getline(cin, CreatedUserName);
 
-
-	UsersAccount user(ID, UserName, Password);
-
-	//brings up the Login
-	cout << "\nLogin:\n";
-
-
-	while (true)
-	{
-		string CreatedUserName, CreatedPassword;
+        // Copy paste but for Password instead
+        cout << "Enter Password: ";
+        getline(cin, CreatedPassword);
 
 
-		//UserName Login
-		cout << "Enter Username : ";
-		getline(cin, CreatedUserName);
-
-		//copy paste but for Password instead
-		cout << "Enter Password : ";
-		getline(cin, CreatedPassword);
-
-
-
-		if (user.Login(CreatedUserName, CreatedPassword))  //
-		{
-			cout << "Login Succesful !\n";
-			cout << "Account ID:" << user.getID() << "\n";
-				break;
-		}
-		else
-		{
-
-			cout << "Login Error, Invalid Username or Password. Try again.\n";
-		}
-	}
-	return 0;
+        // basivally if everything is correct
+        if (user.Login(CreatedUserName, CreatedPassword))
+        {
+            cout << "Login Successful!\n";
+            cout << "Account ID: " << user.getID() << "\n";
+            break;
+        }
+        else
+        {
+            cout << "Login Error, Invalid Username or Password. Try again.\n";
+        }
+    }
+    return 0;
 }
-
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-
